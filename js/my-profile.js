d3adbb6451f375d19918b60ea7d99282a0cf8bae
verdeverdad <sofia.16d@gmail.com>
2021-10-29 19:32:44 -0300

    modificar

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,31 +1,36 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+let nameRegistrado = document.getElementById("nomUsu")
+let nombre = document.getElementById("nombre")
+let edad = document.getElementById("edad")
+let email = document.getElementById("email")
+let direccion = document.getElementById("direccion")
+let nContacto = document.getElementById("numContacto")
+
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
 
 
-    let nameRegistrado = document.getElementById("nomUsu")
+    
     let nameRegistradoLogueado = JSON.parse(localStorage.getItem("nameRegistrado")) //traigo lo que quedo guardado en el set
-    nameRegistrado.innerHTML = "Nombre de usuario: " + nameRegistradoLogueado.nameRegistrado
+    nameRegistrado.innerHTML = nameRegistradoLogueado.nameRegistrado
 
-    let nombre = document.getElementById("nombre")
+    
     let nombreLogueado = JSON.parse(localStorage.getItem("nombre")) //traigo lo que quedo guardado en el set
     nombre.innerHTML = "Nombre completo: " + nombreLogueado.nombre
 
-    let edad = document.getElementById("edad")
     let edadLogueado = JSON.parse(localStorage.getItem("edad")) //traigo lo que quedo guardado en el set
     edad.innerHTML = "Edad: " + edadLogueado.edad
 
-    let email = document.getElementById("email")
     let emailLogueado = JSON.parse(localStorage.getItem("email")) //traigo lo que quedo guardado en el set
     email.innerHTML = "Email: " + emailLogueado.email
 
-    let direccion = document.getElementById("direccion")
     let direccionLogueado = JSON.parse(localStorage.getItem("direccion")) //traigo lo que quedo guardado en el set
     direccion.innerHTML = "Dirección: " + direccionLogueado.direccion
 
 
-    let nContacto = document.getElementById("numContacto")
     let nContactoLogueado = JSON.parse(localStorage.getItem("nContacto")) //traigo lo que quedo guardado en el set
     nContacto.innerHTML = "Número de contacto: " + nContactoLogueado.nContacto
    
@@ -33,30 +38,23 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 function modificar(){
     
-    
-    let nameRegistrado = document.getElementById("nomUsu")
-    let nameRegistradoLogueado = JSON.parse(localStorage.getItem("nameRegistrado")) //traigo lo que quedo guardado en el set
-    nameRegistrado.innerHTML = "Nombre de usuario: " + nameRegistradoLogueado.nameRegistrado
+    nomUsu.contentEditable = "true"
+    nombre.contentEditable = "true";
+    edad.contentEditable = "true"
+    email.contentEditable = "true"
+    direccion.contentEditable = "true"
+    nContacto.contentEditable = "true"
 
-    let nombre = document.getElementById("nombre")
-    let nombreLogueado = JSON.parse(localStorage.getItem("nombre")) //traigo lo que quedo guardado en el set
-    nombre.innerHTML = "Nombre completo: " + nombreLogueado.nombre
+    let bMod = document.getElementById("modificarP");
+    bMod.style.display = "none"
 
-    let edad = document.getElementById("edad")
-    let edadLogueado = JSON.parse(localStorage.getItem("edad")) //traigo lo que quedo guardado en el set
-    edad.innerHTML = "Edad: " + edadLogueado.edad
+    let bguardar = document.getElementById("bguardar");
+    bguardar.style.display = "inline-block";
 
-    let email = document.getElementById("email")
-    let emailLogueado = JSON.parse(localStorage.getItem("email")) //traigo lo que quedo guardado en el set
-    email.innerHTML = "Email: " + emailLogueado.email
+}
 
-    let direccion = document.getElementById("direccion")
-    let direccionLogueado = JSON.parse(localStorage.getItem("direccion")) //traigo lo que quedo guardado en el set
-    direccion.innerHTML = "Dirección: " + direccionLogueado.direccion
+function guardarModificados(){
+    console.log(nomUsu.innerHTML)
 
-
-    let nContacto = document.getElementById("numContacto")
-    let nContactoLogueado = JSON.parse(localStorage.getItem("nContacto")) //traigo lo que quedo guardado en el set
-    nContacto.innerHTML =  nContactoLogueado.nContacto
-    window.location.href = "Perfil.html"
+    localStorage.setItem("nameRegistrado", JSON.stringify({ nameRegistrado: nomUsu.innerHTML }));
 }
