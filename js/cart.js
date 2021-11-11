63f9f6fc68a2f72ca8b3edd8ae343fcff110e575
verdeverdad <sofia.16d@gmail.com>
2021-11-10 23:29:37 -0300

    entrega_7

--- js/cart.js
+++ js/cart.js
@@ -116,41 +116,151 @@ document.addEventListener("DOMContentLoaded", function (e) {
     });
 });
 
-let form = document.getElementById("datosEnvio");
+/*let form = document.getElementById("datosEnvio");
 let pago = document.getElementById("pago");
 var camposCompletos = document.getElementsByClassName("form-control");
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function (event) {
     for (let i = 0; i < camposCompletos.length; i++) {
-   if(camposCompletos[i].value != undefined){
-    pago.style.display = "inline-block";
-   } 
-} 
+        if (camposCompletos[i].value != undefined) {
+            pago.style.display = "inline-block";
+        }
+    }
+});*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("datosEnvio").addEventListener('submit', validarFormulario);
 });
 
-/*
-function validarEnvio() {
-   e.preventDefault();
+function validarFormulario() {
+    event.preventDefault();
     let metodo = document.getElementById("metodo").checked;
-    let fname = document.getElementById("fname");
-    let emailenvio = document.getElementById("emailenvio");
-    let adr = document.getElementById("adr");
-    let numpuerta = document.getElementById("numpuerta");
-    let city = document.getElementById("city");
-    let numcontacto = document.getElementById("numcontacto");
-    let pago = document.getElementById("pago");
     let retiro = document.getElementById("retiro").checked;
+    if (metodo == false && retiro == false) {
+        alert('No has eseleccionado ningun método de envío');
+        return;
+    }
+    if (metodo == true) {
+        let fname = document.getElementById("fname").value;
+        if (fname.length == 0) {
+            alert("Debes completar tu nombre");
+            return;
+        }
+        let emailenvio = document.getElementById("emailenvio").value;
+        if (emailenvio.length == 0) {
+            alert('Debes confirmr tu email');
+            return;
+        }
+        let adr = document.getElementById("adr").value;
+        if (adr.length == 0) {
+            alert('No has proporcionado una dirección de envio');
+            return;
+        }
+        let numpuerta = document.getElementById("numpuerta").value;
+        if (numpuerta.length == 0) {
+            alert('Agrega mas detalles  tu dirección');
+            return;
+        }
+        let city = document.getElementById("city").value;
+        if (city.length == 0) {
+            alert('Proporciona una ciudad');
+            return;
+        }
+        let departamento = document.getElementById("departamento").value;
+        if (departamento.length == 0) {
+            alert('Debes seleccionar un país');
+            return;
+        }
+        let pais = document.getElementById("pais").value;
+        if (pais.length == 0) {
+            alert('Debes seleccionar un país');
+            return;
+        } let numcontacto = document.getElementById("numcontacto").value;
+        if (numcontacto.length == 0) {
+            alert('Confirma tu número de contacto');
+            return;
+        }
+        let premium = document.getElementById("premium").checked;
+        let express = document.getElementById("express").checked;
+        let standard = document.getElementById("standard").checked;
+        if (premium == false && express == false && standard == false) {
+            alert('Confirma un tipo de envío');
+            return;
+        }
 
-    if (metodo == true && fname != undefined && emailenvio != undefined && adr != undefined && numpuerta != undefined && city != undefined && numcontacto != undefined) {
-
-        localStorage.setItem("adr", JSON.stringify({ adr: adr }));
-        localStorage.setItem("numpuerta", JSON.stringify({ numpuerta: numpuerta }));
-        localStorage.setItem("city", JSON.stringify({ city: city }));
-        localStorage.setItem("numcontacto", JSON.stringify({ numcontacto: numcontacto }));
-
+        let pago = document.getElementById("pago");
         pago.style.display = "inline-block";
     }
     if (retiro == true) {
-        pago.style.display = "inline-block";
+        let premium = document.getElementById("premium").checked;
+        let express = document.getElementById("express").checked;
+        let standard = document.getElementById("standard").checked;
+        if (premium == false && express == false && standard == false) {
+            alert('Confirma un tipo de envío');
+            return;
+        }
+        else {
+            pago.style.display = "inline-block";
+            /* localStorage.setItem("adr", JSON.stringify({ adr: adr }));
+             localStorage.setItem("numpuerta", JSON.stringify({ numpuerta: numpuerta }));
+             localStorage.setItem("city", JSON.stringify({ city: city }));
+             localStorage.setItem("numcontacto", JSON.stringify({ numcontacto: numcontacto }));*/
+        }
     }
 }
-*/
+
+function validarPago() {
+    event.preventDefault();
+    let transferencia = document.getElementById("transferencia").checked;
+    let terminos = document.getElementById("terminos").checked;
+    let tj = document.getElementById("tj").checked;
+
+    if (transferencia == false && tj == false) {
+        alert('No has eseleccionado ningun método de pago');
+        return;
+    }
+    if (transferencia == true) {
+        let ccnum = document.getElementById("ccnum").value;
+        if (ccnum.length == 0) {
+            alert("Debes completar el número de transferencia");
+            return;
+        }
+        if (terminos == false) {
+            alert("Debes leer los términos y condiciones");
+            return;
+        }
+      else( alert("felicitaciones"))
+    }
+    if(tj == true){
+        let ccnumtj = document.getElementById("ccnumtj").value;
+        if (ccnumtj.length == 0) {
+            alert("Debes completar el número de tarjeta");
+            return;
+        } let mestj = document.getElementById("mestj").value;
+        if (mestj.length == 0) {
+            alert("Debes completar el mes");
+            return;
+        } let añotj = document.getElementById("añotj").value;
+        if (añotj.length == 0) {
+            alert("Debes completar el año");
+            return;
+        } let expaño = document.getElementById("expaño").value;
+        if (expaño.length == 0) {
+            alert("Debes completar el año que expira");
+            return;
+        } let cvv = document.getElementById("cvv").value;
+        if (cvv.length == 0) {
+            alert("Debes completar el código de seguridad");
+            return;
+        }
+        
+        if (terminos == false) {
+            alert("Debes leer los términos y condiciones");
+            return;
+        }
+        else( alert("felicitaciones"))
+    }
+}
+
+
+
+
